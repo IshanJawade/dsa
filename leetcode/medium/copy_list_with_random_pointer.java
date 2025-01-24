6a16501839e8624716cc727245ed6974eb25809c
IshanJawade <ishankjawade@gmail.com>
2025-01-24 11:16:51 -0800

    remove nth node added

---
+++ leetcode/medium/copy_list_with_random_pointer.java
@@ -0,0 +1,7 @@
+package leetcode.medium;
+
+public class copy_list_with_random_pointer {
+	public static void main(String[] args) {
+		
+	}
+}
--- leetcode/medium/remove_nth_node.java
+++ leetcode/medium/remove_nth_node.java
@@ -6,27 +6,64 @@ public class remove_nth_node {
         int val;
         ListNode next;
 
+        // optimized solution (Two pointer method)
         public ListNode removeNthFromEnd(ListNode head, int n) {
+            // using dummy node to overcome eadge cases
+            ListNode dummy = new ListNode(0);
+            dummy.next = head;
+            ListNode fast  = dummy;
+            ListNode slow  = dummy;
+
+            for(int i=0; i<n; i++){
+                fast = fast.next;
+            }
+            // moving fast one step ahead (can't do it in the loop)
+            fast = fast.next;
+
+            while(fast != null){
+                fast = fast.next;
+                slow = slow.next;
+            }
+
+            slow.next = slow.next.next;
+            // returning dummy.next node because of first step
+            return dummy.next;
+        }
+
+        // brut force solution but still worked 100%
+        public ListNode removeNthFromEndbr(ListNode head, int n) {
             ListNode current = head;
             int count = 0;
+
+            // count the number of nodes
             while(current != null){
                 count++;
+                current = current.next;
             }
+            // where to stop for deletion (one node before)
             int stopNode = count - n;
 
-            current =head;
+            // edge cases
+            if (n > count) return head;
+            if(stopNode == 0){
+                return head.next;
+            }
+
+            current = head;
             count = 0;
             ListNode prev = null;
-            ListNode temp = null;
-            while(current != null){
-                if(count == stopNode){
-                    prev = current;
-                    prev.next = current.next.next;
-                    break;
-                }
-                count++;
+
+            //trverse till the one node before deletion node
+            while(count < stopNode){
+               prev = current;
+               current = current.next;
+               count++;
             }
-            return prev;
+
+            //skip the node and link the next node (deletion)
+            prev.next = current.next;
+
+            return head;
         }
     }
 
