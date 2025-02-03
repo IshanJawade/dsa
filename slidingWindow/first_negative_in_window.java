4c8d4c71360ba4faa9311681bb4e265eb175ebde
IshanJawade <ishankjawade@gmail.com>
2025-02-02 16:00:34 -0800

    First Negative Number code completed

--- slidingWindow/first_negative_in_window.java
+++ slidingWindow/first_negative_in_window.java
@@ -1,10 +1,11 @@
 package slidingWindow;
 
-import java.util.*;
-
 public class first_negative_in_window {
 	static int[] first_nagative_window(int[] arr, int k){
-		ArrayList <Integer> negatives = new ArrayList<>();
+		// create array to store first negative numbers
+		// formula: array size = size of given array - (window size + 1) 
+		int[] negatives = new int[(arr.length - k)+1];
+		int nptr = 0;
 
 		// create window
 		int i=0, j=0;
@@ -13,20 +14,28 @@ public class first_negative_in_window {
 		}
 		// sliding window through array
 		while(j < arr.length){
-			if(arr[i] < 0){
-				negatives.add(arr[i]);
-				i++;
-				continue;
-			} else {
-				i++;
+			int cnt = i;
+			while(cnt != j){
+				if(arr[cnt] < 0){
+					negatives[nptr] = arr[cnt];
+					nptr ++;
+					break;
+				}
+				cnt++;
 			}
+			i++;
+			j++;
 		}
-
+		return negatives;
 	}
 	public static void main(String[] args) {
 		int[] arr = {12,-1,-7,8,-15, 30,16,28};
 		int windowSize = 3;
 		int[] firstNegative = first_nagative_window(arr, windowSize);
-		System.err.println(firstNegative);
+		
+		for (int i : firstNegative) {
+			System.out.print(i + " ");
+		}
+		System.out.println("");
 	}
 }
