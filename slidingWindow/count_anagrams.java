c6f7cdbc21b5c0fde9cdd6bdf86d5a323cf32a5f
IshanJawade <ishankjawade@gmail.com>
2025-02-03 18:04:27 -0800

    count_anagram update

--- slidingWindow/count_anagrams.java
+++ slidingWindow/count_anagrams.java
@@ -1,7 +1,15 @@
 package slidingWindow;
 
+import java.util.*;
+
 public class count_anagrams {
+    static int countAnagrams(String s){
+        Map<Character, Integer> map = new HashMaps<>();
+
+        return 0;
+    }
     public static void main(String[] args) {
-        
+        String str = "aabaaabaabb";
+        System.out.println(countAnagrams(str));
     }
 }
