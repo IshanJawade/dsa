71e5d150c2bb3eeee54291e20ed5caa0e653b052
IshanJawade <ishankjawade@gmail.com>
2025-04-02 10:59:11 -0700

    subsequesnce code added

---
+++ recurssion/subsequence.java
@@ -0,0 +1,29 @@
+package recurssion;
+
+import java.util.*;
+
+public class subsequence {
+	
+	static void giveSubSquence(int ind, ArrayList<Integer> list, int[] nums, int n){
+		// edge cases
+		if(ind >= n){
+			System.out.println(list);
+			return;
+		}
+		if(n == 0) System.out.println("{}");
+
+		// Take or pick the perticular index
+		list.add(nums[ind]);
+		giveSubSquence(ind+1, list, nums, n);
+		list.removeLast();
+
+		// Not pick or not take perticular index
+		giveSubSquence(ind+1, list, nums, n);
+	}
+	public static void main(String[] args) {
+		int[] nums = {3,1,2};
+		int n = nums.length;
+		ArrayList <Integer> list = new ArrayList<>();
+		giveSubSquence(0, list, nums, n);
+	}
+}
