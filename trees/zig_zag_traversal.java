dae969ddb4558174204ff5dfdb6943243c79f74f
IshanJawade <ishankjawade@gmail.com>
2025-05-07 12:32:21 -0700

    halfbaked zig zag traversal code

---
+++ trees/zig_zag_traversal.java
@@ -0,0 +1,22 @@
+package trees;
+
+public class zig_zag_traversal {
+	class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        
+        TreeNode(int n) {
+            this.val = n;
+            this.left = null;
+            this.right = null;
+        }
+    
+    }
+
+	
+
+	public static void main(String[] args) {
+		
+	}
+}
