4dc91cff17337ccc1ecdcdf2df5e310c1cd2d345
IshanJawade <ishankjawade@gmail.com>
2025-09-04 19:42:30 -0700

    BFS code added for graph

--- graphs/dfs_bfs.java
+++ graphs/dfs_bfs.java
@@ -5,35 +5,57 @@ import java.util.*;
 public class dfs_bfs {
 
     // Create an adjecency matrix
-    static List<List<Integer>> createGraph(int V, int[][] edges){
-        List<List<Integer>> graph = new ArrayList<>();
+    static List<List<Integer>> createAdjMatrix(int V, int[][] edges){
+        List<List<Integer>> adj = new ArrayList<>();
 
         // first make empty array lists in graph list
         // this loop will add V number of empty arraylists in graph
         for(int i=0; i<V; i++){
-            graph.add(new ArrayList<>());
+            adj.add(new ArrayList<>());
         }
         // int[][] = { {first []} second []}
         for(int[] e : edges){
             int u = e[0];   // source
             int v = e[1];   // destination
 
-            // take u'th arraylist from graph add (int) v in it
-            graph.get(u).add(v); 
+            // take u'th arraylist from adj add (int) v in it
+            adj.get(u).add(v); 
         }
-        return graph;
+        return adj;
     }
 
-    static void DFS (List<List<Integer>>graph, int u, boolean[] visited, List<Integer> dfs){
+    // DFS Algorithm (recursive)
+    static void DFS (List<List<Integer>>adj, int u, boolean[] visited, List<Integer> result){
         // base case 
         if(visited[u]) return;
 
         visited[u] = true;
-        dfs.add(u);
+        result.add(u);
 
-        for(int v: graph.get(u)){
+        for(int v: adj.get(u)){
             if(!visited[v]){
-                DFS(graph, v, visited, dfs);
+                DFS(adj, v, visited, result);
+            }
+        }
+    }
+
+    // BFS Algorithm (linear)
+    static void BFS(List<List<Integer>>adj, int start, boolean[] visited, List<Integer> result){
+        Queue<Integer> que = new ArrayDeque<>();
+
+        visited[start] = true;
+        que.add(start);
+        result.add(start);  
+        
+        while(!que.isEmpty()){
+            int u = que.poll();
+
+            for(int v : adj.get(u)){
+                if(!visited[v]){
+                    visited[v] = true;
+                    que.add(v);
+                    result.add(v);
+                }
             }
         }
     }
@@ -48,17 +70,18 @@ public class dfs_bfs {
         };
 
         // Boolean array to track of visited nodes
-        boolean[] visited = new boolean[V];
+        boolean[] visitedDfs = new boolean[V];
+        boolean[] visitedBfs = new boolean[V];
 
-        List<List<Integer>> graph = createGraph(V, edges);
-        System.err.println(graph);
+        List<List<Integer>> adj = createAdjMatrix(V, edges);
+        System.err.println(adj);
 
         List<Integer> dfs = new ArrayList<>();
-        DFS(graph, 0, visited, dfs);
+        DFS(adj, 0, visitedDfs, dfs);
         System.err.println(dfs);
 
-        // List<Integer> bfs = new ArrayList<>();
-        // BFS(graph, 0, visited, bfs);
-        // System.err.println(bfs);
+        List<Integer> bfs = new ArrayList<>();
+        BFS(adj, 0, visitedBfs, bfs);
+        System.err.println(bfs);
     }
 }
